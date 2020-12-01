0eb315ffaabe4c3a4301c5ec761f2680bf93e2a9
Jean Paul Ferreira <jeanpaul@jepafe.net>
2020-12-01 20:40:49 -0300

    SignIn

--- app.js
+++ app.js
@@ -4,43 +4,19 @@ const exphbs = require('express-handlebars')
 
 const port = process.env.PORT || 3000
 
+const flash = require('express-flash')
+app.use(flash())
+
 const session = require('express-session')
 app.use(session({
     secret: 'Secreto',
     resave: false,
-    saveUninitialized: true,
-    cookie: { secure: true }
+    saveUninitialized: false
 }))
-
-const bcrypt = require('bcrypt')
-
-const connection = require('./database')
 
 const passport = require('passport')
-const LocalStrategy = require('passport-local').Strategy
 
-passport.use('local.signup', new LocalStrategy({
-    usernameField: 'email',
-    passwordField: 'password'
-}, async (username, password, done) => {
-    const salt = await bcrypt.genSalt(10)
-    const hash = await bcrypt.hash(password, salt)
-
-    const user = { email:username, password:hash }
-
-    const result = await connection.query('INSERT INTO users SET ?', user)
-    user.id = result.insertId
-    done(null, user)
-}))
-
-passport.serializeUser((user, done) => {
-    done(null, user.id)
-})
-
-passport.deserializeUser( async (id, done) => {
-    const rows = await connection.query(`SELECT * FROM users WHERE id = ${id}`)
-    done(null, rows[0])
-})
+require('./passport')
 
 app.use(passport.initialize())
 app.use(passport.session())
