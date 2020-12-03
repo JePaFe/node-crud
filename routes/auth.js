ce63d607142378c1d31f757d2582aad326f489e1
Jean Paul Ferreira <jeanpaul@jepafe.net>
2020-12-03 20:31:34 -0300

    Validator

--- routes/auth.js
+++ routes/auth.js
@@ -1,13 +1,51 @@
 const express = require('express')
 const router = express.Router()
 
+const { body, validationResult } = require('express-validator')
+
+const connection = require('../database')
+
 const passport = require('passport')
 
 router.get('/signup', (req, res) => {
     res.render('auth/signup')
 })
 
-router.post('/signup', passport.authenticate('local.signup', {
+router.post('/signup', [
+    body('email')
+        .isEmail().withMessage('El correo es obligatorio')
+        .normalizeEmail(),
+
+    body('email').custom(async value => {
+        const rows = await connection.query(`SELECT * FROM users WHERE email = '${value}'`)
+
+        if (rows.length > 0) {
+            return Promise.reject('El correo ya esta registrado')
+        }
+    }),
+
+    body('password')
+        .isLength(5).withMessage('La contraseña tiene que tener al menos 5 caracteres'),
+
+    body('password_confirmation').custom((value, {req}) => {
+        if(req.body.password !== value) {
+            throw new Error('Las contraseña no coinciden')
+        }
+
+        return true
+    })
+], (req, res, next) => {
+    const errors = validationResult(req)
+
+    if(!errors.isEmpty()) {
+        return res.render('auth/signup', {
+            errors: errors.array(),
+            email: req.body.email
+        })
+    }
+
+    return next()
+}, passport.authenticate('local.signup', {
     successRedirect: '/profile',
     failureRedirect: '/signup'
 }))
@@ -22,7 +60,25 @@ router.get('/signin', (req, res, next) => {
     res.render('auth/signin')
 })
 
-router.post('/signin', passport.authenticate('local.signin', {
+router.post('/signin', [
+    body('email')
+        .isEmail().withMessage('El correo es obligatorio')
+        .normalizeEmail(),
+
+    body('password')
+        .isLength(5).withMessage('La contraseña tiene que tener al menos 5 caracteres'),
+], (req, res, next) => {
+    const errors = validationResult(req)
+
+    if(!errors.isEmpty()) {
+        return res.render('auth/signin', {
+            errors: errors.array(),
+            email: req.body.email
+        })
+    }
+
+    return next()
+}, passport.authenticate('local.signin', {
     successRedirect: '/profile',
     failureRedirect: '/signin',
     failureFlash: true
